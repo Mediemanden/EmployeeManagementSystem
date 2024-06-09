6081a6b177ed79ed8adb1d2953429588bcf74d31
Christoffer Bredo Lillelund <ChristofferBredo@gmail.com>
2024-06-09 16:46:17 +0200

    Add Unit test project and unit tests for employee creation

---
+++ Employee.Business.UnitTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+using System;
+using Employee.Business.Commands;
+using Employee.Business.Models;
+using Employee.DataAccess.Interfaces;
+using NSubstitute;
+
+namespace Employee.Business.UnitTest;
+
+public class CreateEmployeeTests
+{
+    private IEmployeeStorage _employeeStorageMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        _employeeStorageMock = Substitute.For<IEmployeeStorage>();
+    }
+
+    [Test]
+    public void CreateNewEmployee_WithEmptyName_ExpectError1401()
+    {
+        // Testdata 
+        EmployeeModel employee = new()
+        {
+            Id = Guid.NewGuid(),
+            FullName = "", // Empty name
+            DateOfBirth = DateTime.Now.AddYears(-20),
+            Salary = 1000,
+            Department = "IT"
+        };
+
+        // Setup
+        var command = new CreateEmployeeCommand(_employeeStorageMock);
+
+        // Act and Assert
+        EmsException ex = Assert.ThrowsAsync<EmsException>(() => command.ExecuteAsync(employee));
+
+        Assert.That(ex.Code, Is.EqualTo(ExceptionCodes.EmployeeNameRequired__1401));
+    }
+
+    [Test]
+    public void CreateNewEmployee_EmployeeTooYoung_ExpectError1402()
+    {
+        // Testdata 
+        EmployeeModel employee = new()
+        {
+            Id = Guid.NewGuid(),
+            FullName = "John Doe",
+            DateOfBirth = DateTime.Now.AddYears(-1), // Too young
+            Salary = 1000,
+            Department = "IT",
+        };
+
+        // Setup
+        var command = new CreateEmployeeCommand(_employeeStorageMock);
+
+        // Act and Assert
+        EmsException ex = Assert.ThrowsAsync<EmsException>(() => command.ExecuteAsync(employee));
+
+        Assert.That(ex.Code, Is.EqualTo(ExceptionCodes.EmployeeAgeUnder16__1402));
+    }
+}
